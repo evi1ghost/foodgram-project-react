080274a0fab95cd9c947b69ab6052dce40ed56c3
Дубинчик Андрей <evil_ghost@bk.ru>
2021-10-29 02:23:34 +0300

    base structure

---
+++ backend/apps/users/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+
+
+class User(AbstractUser):
+    first_name = models.CharField(_('first name'), max_length=150)
+    last_name = models.CharField(_('last name'), max_length=150)
+    email = models.EmailField(_('email address'))
+    REQUIRED_FIELDS = ['email', 'first_name', 'last_name']
+
+    def __str__(self):
+        return f'{self.last_name} {self.first_name}'
