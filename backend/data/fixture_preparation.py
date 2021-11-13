b38c3724aa3f20c3f09ba96423b42bbcfa38c60d
Дубинчик Андрей <evil_ghost@bk.ru>
2021-11-14 01:59:59 +0300

    migrations collapsed, script for fixtures added

---
+++ backend/data/fixture_preparation.py
@@ -0,0 +1,23 @@
+import json
+
+
+def add_parametrs(ingredient, id):
+    fixture = {
+        "model": "recipes.ingredient",
+        "pk": id,
+        "fields": ingredient
+    }
+    return fixture
+
+
+def create_fixtures():
+    with open('ingredients.json', 'r') as f:
+        ingredients = json.loads(f.read())
+        for i in range(len(ingredients)):
+            ingredients[i] = add_parametrs(ingredients[i], i + 1)
+    with open('fixtures.json', 'w') as f:
+        f.write(json.dumps(ingredients))
+
+
+if __name__ == '__main__':
+    create_fixtures()
---
+++ backend/loaddata.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+cd data/
+python3 fixture_preparation.py
+cd ..
+python3 manage.py loaddata data/fixtures.json
